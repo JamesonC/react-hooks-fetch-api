404ccad2f156645571c0e6d19506e7cc13a41ec5
Jameson Campbell <jcampbell@flexterapp.com>
2020-08-05 13:16:51 -0500

    network request logging response data to console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://hn.algolia.com/api/v1/search?query=reacthooks');
+
+      result
+        .json()
+        .then(result => setData(result))
+        .catch(e => console.log(e))
+    }
+    fetchData()
+  }, []);
+
+  console.log(data) 
+
   return (
     <div className="container">
       <h1>Fetch Data with React Hooks</h1>
@@ -18,7 +35,7 @@ const App = () => {
               <td>Title 1</td>
               <td>Author 1</td>
               <td>
-                <a class="button" target='_blank' href='https://www.google.com/'>Read</a>
+                <a className="button" target='_blank' href='https://www.google.com/' rel="noopener noreferrer">Read</a>
               </td>
             </tr>
           </tbody>
